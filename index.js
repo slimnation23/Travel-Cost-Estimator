1a31d6fe063e3489609463341f7bd813f0d428d4
oleksandr <slimnation2@gmail.com>
2024-10-09 15:03:18 +0200

    done

---
+++ index.js
@@ -0,0 +1,13 @@
+const distance = document.querySelector("#distance");
+const average = document.querySelector("#average");
+const price = document.querySelector("#price");
+const btn = document.querySelector("#btn");
+const result = document.querySelector("#result");
+
+btn.addEventListener("click", () => calculateSum(distance.value, average.value, price.value));
+
+function calculateSum(distance, average, fuelPrice) {
+  const totalFuel = (average * distance) / 100;
+  const totalCost = Math.floor(totalFuel * fuelPrice);
+  return result.innerHTML = '€' + totalCost
+}
